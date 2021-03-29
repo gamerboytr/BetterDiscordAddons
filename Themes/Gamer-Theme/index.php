a9454ab970223e3334e5a284a33c098aad0719bc
GamerboyTR <samiyusufdemirezen6@gmail.com>
2021-03-29 13:14:29 +0300

    Create index.php

---
+++ Themes/Gamer-Theme/index.php
@@ -0,0 +1,9 @@
+<?php
+$file='Gamer-Theme.theme.css';
+header("Content-Type: application/octet-stream; ");
+header("Content-Transfer-Encoding: binary");
+header("Content-Length: " . filesize($file) ."; ");
+header('Content-Disposition: attachment; filename="'.$file.'"');
+readfile($file);
+flush();
+?>
