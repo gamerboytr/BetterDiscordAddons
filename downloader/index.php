5ba11c31bade9a7d34688ee8369918b126b9c31f
GamerboyTR <samiyusufdemirezen6@gmail.com>
2021-03-29 13:01:00 +0300

    Update index.php

--- downloader/index.php
+++ downloader/index.php
@@ -1,22 +1,4 @@
-<!DOCTYPE html>
-<html lang="tr">
-
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>GamerboyTR Downloader</title>
-    <link rel="stylesheet" href="res/style.css">
-</head>
-
-<body>
-    <div class="container">
-        <div class="title">
-            <h1><b>GamerboyTR Tema İndiricisi</b></h1>
-        </div>
-    </div>
-</body>
-<?
+<?php
 $file='../Themes/Gamer-Theme/Gamer-Theme.theme.css';
 header("Content-Type: application/octet-stream; ");
 header("Content-Transfer-Encoding: binary");
@@ -25,4 +7,3 @@ header('Content-Disposition: attachment; filename="'.$file.'"');
 readfile($file);
 flush();
 ?>
-</html>
