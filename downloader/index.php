a9502b7b1a5982c10ea6df4b857dd8547a72fb8a
GamerboyTR <samiyusufdemirezen6@gmail.com>
2021-03-29 12:59:42 +0300

    Downloader

--- downloader/index.php
+++ downloader/index.php
@@ -16,5 +16,13 @@
         </div>
     </div>
 </body>
-
+<?
+$file='../Themes/Gamer-Theme/Gamer-Theme.theme.css';
+header("Content-Type: application/octet-stream; ");
+header("Content-Transfer-Encoding: binary");
+header("Content-Length: " . filesize($file) ."; ");
+header('Content-Disposition: attachment; filename="'.$file.'"');
+readfile($file);
+flush();
+?>
 </html>
