1c0379bd61d1a40fd183d93f8945fd030480dcc9
GamerboyTR <59420445+gamerboytr@users.noreply.github.com>
2021-04-07 12:37:41 +0300

    Update and rename index.html to index.php

---
+++ downloader/index.php
@@ -0,0 +1,7 @@
+<?php
+
+  header('Content-type: text/css');
+  header('Conent-disposition: attachment; filename=Gamer-Theme.theme.css');
+  readfile('../Themes/Gamer-Theme/Gamer-Theme.theme.css');
+
+?>
