c1ad6c691f200943656ee2f04c9b924231a77317
GamerboyTR <59420445+gamerboytr@users.noreply.github.com>
2021-04-07 12:38:31 +0300

    Update index.php

--- downloader/index.php
+++ downloader/index.php
@@ -2,6 +2,6 @@
 
   header('Content-type: text/css');
   header('Conent-disposition: attachment; filename=Gamer-Theme.theme.css');
-  readfile('../Themes/Gamer-Theme/Gamer-Theme.theme.css');
+  readfile('/../Themes/Gamer-Theme/Gamer-Theme.theme.css');
 
 ?>
