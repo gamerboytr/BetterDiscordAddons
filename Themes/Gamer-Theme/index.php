a5e267e3ed41422ed1a230cb1bc90d4107294c76
GamerboyTR <59420445+gamerboytr@users.noreply.github.com>
2021-04-27 16:44:55 +0300

    Delete index.php

--- Themes/Gamer-Theme/index.php
+++
@@ -1,9 +0,0 @@
-<?
-$file='Gamer-Theme.theme.css';
-header("Content-Type: application/octet-stream; ");
-header("Content-Transfer-Encoding: binary");
-header("Content-Length: " . filesize($file) ."; ");
-header('Content-Disposition: attachment; filename="'.$file.'"');
-readfile($file);
-flush();
-?>
